6f24cd0f62f692f28f75782ba963bdfa2fd41d39
mor mbathie <mormbathie98@gmail.com>
2026-05-04 18:00:05 +0100

    Test WebHooks12

--- src/students/students.controller.ts
+++ src/students/students.controller.ts
@@ -12,7 +12,7 @@ export class StudentsController {
   constructor(private readonly studentsService: StudentsService) { }
   
   @Post()
-  @Roles('GESTIONNAIRE','ADMIN','TEACHER')
+  @Roles('GESTIONNAIRE','ADMIN')
   create(@Body() dto: CreateStudentDto) {
     return this.studentsService.create(dto);
   }
